30266801b8aa19a6ba76659f071082f517a2e2a9
gara0xq <gara.xq@gmail.com>
2024-04-06 04:33:00 +0200

    garaxq

--- EgyptPost/Authin/urls.py
+++ EgyptPost/Authin/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('register/', RegisterView.as_view(), basename='register')
+router.register('login/', LoginView.as_view(), basename='login')
+router.register('user/', UserView.as_view(), basename='user')
+router.register('logout/', LogoutView.as_view(), basename='logout')
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('user/', UserView.as_view()),
-    path('logout/', LogoutView.as_view()),
+    path('', include(router.urls))
 ]
--- EgyptPost/EgyptPost/urls.py
+++ EgyptPost/EgyptPost/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('admin/', admin.site.urls, basename='admin')
+router.register('auth/', include('Authin.urls'), basename='auth')
+router.register('technicalSupport/', include('TechnicalSupport.urls'), basename='technicalSupport')
+router.register('messages/', include('Files.urls'), basename='messages')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('Authin.urls')),
-    path('technicalSupport/', include('TechnicalSupport.urls')),
-    path('messages/', include('Files.urls')),
+    path('', include(router.urls))
 ]
